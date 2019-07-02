a9bd70309248ad1c833e9c3ea952e44343165725
SaiChandra2896 <saichandra2896@gmail.com>
2019-07-02 14:15:33 +0530

    hash password and save while registering

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
 const keys = require('../../config/keys');
 
@@ -27,9 +28,19 @@ router.post('/register', (req, res) => {
         email: req.body.email,
         password: req.body.password
       });
-      newUser.save().then(user => res.json(user)).catch(err => console.log(err));
+
+      bcrypt.genSalt(10, (err, salt) => {
+        //console.log(salt);
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) {
+            throw err;
+          }
+          newUser.password = hash;
+          newUser.save().then(user => res.json(user)).catch(err => console.log(err));
+        });
+      });
     }
-  })
+  });
 });
 
 module.exports = router;
