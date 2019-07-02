458a1d133748243d9677262120942e6f3041b73e
SaiChandra2896 <saichandra2896@gmail.com>
2019-07-02 11:46:38 +0530

    add mongodburi and user schema

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://sai:sainani123@oauth-wec27.mongodb.net/test?retryWrites=true&w=majority',
+  secretOrKey: 'secret'
+}
---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//user schema
+const UserSChema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const User = mongoose.model('User', UserSChema);
+
+module.exports = User;
