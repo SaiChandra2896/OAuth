8c2055622f64522a7d8d8002f694b29adeddbf96
SaiChandra2896 <saichandra2896@gmail.com>
2019-07-03 14:42:01 +0530

    add Register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
+import Register from './components/auth/Register';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <Register></Register>
     </div>
   );
 }
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,9 +1,66 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import TextFeild from '../common/TextField';
 class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      password2: '',
+      errors: {}
+    };
+  }
   render() {
-    return (
-      <div>
 
+    const { errors } = this.state;
+
+    return (
+      <div className='register'>
+        {/* to get responsive behaviour */}
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-8 m-auto'>
+              <h1 className='display-4 text-center'>Sign Up</h1>
+              <p className='lead text-center'>Create your Account</p>
+              <form onSubmit={this.onSubmit}>
+                <TextFeild
+                  placeholder="Name"
+                  name="name"
+                  value={this.state.name}
+                  onChange={this.onChange}
+                  error={errors.name}
+                />
+                <TextFeild
+                  placeholder="Email Address"
+                  name="email"
+                  type="email"
+                  value={this.state.email}
+                  onChange={this.onChange}
+                  error={errors.email}
+                  info="Please enter valid Email"
+                />
+                <TextFeild
+                  placeholder="Password"
+                  name="password"
+                  type="password"
+                  value={this.state.password}
+                  onChange={this.onChange}
+                  error={errors.password}
+                />
+                <TextFeild
+                  placeholder="Confirm Password"
+                  name="password2"
+                  type="password"
+                  value={this.state.password2}
+                  onChange={this.onChange}
+                  error={errors.password2}
+                />
+                <input type='submit' className='btn btn-info btn-block mt-4' />
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
